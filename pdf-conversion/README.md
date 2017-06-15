49b050a006be8879bb6f250d300a3b536fcda278
Patrick Louis <patrick@iotek.org>
2017-06-15 21:20:11 +0300

    Added roadmap

--- pdf-conversion/README.md
+++ pdf-conversion/README.md
@@ -3,3 +3,15 @@
 With `node app.js [URL]` this parses the URL to a reading-view, similar to Firefox, using 'readability-node' package.
 
 Uses node lts/boron.
+
+# TODO:
+
+* Have a colon/separator file:
+```
+ARTICLE NAME===INTRO TEXT===URL===SHOULD FETCH CONTENT
+```
+* Fetch the content of the articles that are marked as so
+* Gather every HTML file in a directory
+* Convert the HTML to markdown
+* Join back with the non-fetch content
+* Convert to PDF (maybe: wkhtmltopdf?)
--- pdf-conversion/app.js
+++ pdf-conversion/app.js
@@ -5,19 +5,21 @@ fs = require('fs');
 
 var args = process.argv.slice(2);
 var uri = args[0] || "https://nixers.net/newsletter/feed.xml";
+var outname = args[1] || 'article.html';
 var src = '';
 
 req.get(uri, function(err, res, body){
     if(!err && res.statusCode == 200){
         var doc = jsdom(body, { 
             features: {
-                FetchExternalResources: false,
-                ProcessExternalResources: false
+                FetchExternalResources: true,
+                ProcessExternalResources: true
             }});
 
         var article = new r.Readability(uri, doc).parse();
+        console.log(article);
 
-        fs.writeFile("article.html", '<meta http-equiv="Content-Type" content="text/html; charset=utf-8;"><h1>' + article.title + '</h1>\n\n' + article.content, function(err){
+        fs.writeFile(outname, '<meta http-equiv="Content-Type" content="text/html; charset=utf-8;"><h1>' + article.title + '</h1>\n\n' + article.content, function(err){
             if(err){
                 console.log(err);
             }
